5642aa0a43a6fc8ac23d5560167d7f8fcdb04ddf
Mark Reedman <43639418+MReedman1@users.noreply.github.com>
2018-10-01 20:36:30 +0100

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,18 @@
+# replace these whith your own values!
+my_name = 'Mark Reedman'
+my_age = 19 # maybe
+my_height = 69 # inches
+my_weight = 168 # pounds
+my_eyes = 'Blue'
+my_hair = 'Brown'
+is_heavy = my_weight < 3000
+
+print(f"Let's talk about {my_name}.")
+print(f"He is this {my_age}.")
+print(f"He is {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"It is {is_heavy} that he is normal weight")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
