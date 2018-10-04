f28c6b1cc566b3f8b5c9090169cb1f7b6e2890d5
Mark Reedman <43639418+MReedman1@users.noreply.github.com>
2018-10-04 15:09:13 +0100

    Add files via upload

--- ex6.py
+++ ex6.py
@@ -1,17 +1,38 @@
-my_name = input('Enter your name:')
-my_age = input('Enter your Age:')
-my_height = input('Enter your Height:')
-my_weight = input('Enter your Weight:')
-my_eyes = input('Enter your Eye colour:')
-my_hair = input('Enter your Hair colour:')
+name = input('Enter your Name:')
+age = int(input('Enter your Age:'))
+height = int(input('Enter your Height (specify inches):'))
+weight = int(input('Enter your Weight  (specify kilograms):'))
+eyes = input('Enter your Eye colour:')
+hair = input('Enter your Hair colour:')
 
-
-print(f"Let's talk about {my_name}.")
-print(f"He is this {my_age}.")
-if
-print(f"He is {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy.")
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
-
-total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
+print(f"Let's talk about {name}.")
+print(f"He is this {age} year old.")
+if age > 1 and age < 13:
+    print('This person is a child')
+elif age >= 13 and age < 18:
+    print('This person is a teenager.')
+elif age >= 18:
+    print('The person is an adult.')
+else:
+    print('check that your input is an integer and try again')
+print(f"You are {height} inches tall.")
+if height <= 66:
+    print('Midget')
+elif height > 66 and height < 72:
+    print('Normal height')
+elif height >= 72 and height < 77:
+    print('Tall')
+elif height >=77:
+    print('Giant')
+print(f"He's {weight} pounds heavy.")
+if weight <= 75:
+    print('You are Underweight')
+elif weight > 75 and weight < 100:
+    print('You are at a Healthy weight')
+elif weight >= 100 and weight < 120:
+    print('You are Overweight')
+elif weight >= 120 and weight < 160:
+    print('You are Obese')
+elif weight >=160:
+    print('Very Obese')
+print(f"He's got {eyes} eyes and {hair} hair.")
