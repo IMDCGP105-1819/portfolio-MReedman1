dc4639c40a7966a0fb71419f20a401d75b71c2ce
Mark Reedman <43639418+MReedman1@users.noreply.github.com>
2018-10-04 11:51:48 +0100

    Add files via upload
    v2

--- ex6.py
+++ ex6.py
@@ -1,13 +1,14 @@
 my_name = input('Enter your name:')
-my_age = input('Age:')
-my_height = input('Height:')
-my_weight = input('Weight:')
-my_eyes = input('Eye colour:')
-my_hair = input('Hair colour:')
+my_age = input('Enter your Age:')
+my_height = input('Enter your Height:')
+my_weight = input('Enter your Weight:')
+my_eyes = input('Enter your Eye colour:')
+my_hair = input('Enter your Hair colour:')
 
 
 print(f"Let's talk about {my_name}.")
 print(f"He is this {my_age}.")
+if
 print(f"He is {my_height} inches tall.")
 print(f"He's {my_weight} pounds heavy.")
 print(f"He's got {my_eyes} eyes and {my_hair} hair.")
